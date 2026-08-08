34e6af99d90bba45b8b295d193c41623c88b2b08
Swathi3004 <Swathi.nirmalraj@obiikriationz.com>
2026-08-09 00:15:55 +0530

    fix: Simplify Next.js config for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/Next-js",
-  assetPrefix: "/Next-js/",
+  trailingSlash: true,
 };
 
 export default nextConfig;
